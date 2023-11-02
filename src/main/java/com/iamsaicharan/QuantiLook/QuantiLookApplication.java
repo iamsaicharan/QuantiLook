f6969d5028827a59ac89108b53941614b020607a
iamsaicharan <charanvadakapur@gmail.com>
2023-11-02 10:21:09 -0400

    first commit

---
+++ src/main/java/com/iamsaicharan/QuantiLook/QuantiLookApplication.java
@@ -0,0 +1,13 @@
+package com.iamsaicharan.QuantiLook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantiLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantiLookApplication.class, args);
+	}
+
+}
